c48429f08bc4a405f1d004ca94ef3eb6fcb5ab8e
Emre Tarakci <tarakci.yazilim@gmail.com>
2022-08-29 23:40:32 +0300

    Added clipping mask

--- sketch-skew.js
+++ sketch-skew.js
@@ -12,7 +12,7 @@ const sketch = ({ context, width, height }) => {
   // is executed once
   let x, y, w, h, fill, stroke;
 
-  const num = 50;
+  const num = 100;
   const degrees = 10;
 
   const rects = [];
@@ -28,7 +28,7 @@ const sketch = ({ context, width, height }) => {
   for(let i = 0; i < num; i++) {
     x = random.range(0, width);
     y = random.range(0, height);
-    w = random.range(100, 300);
+    w = random.range(300, 700);
     h = random.range(40, 200);
 
     fill = random.pick(rectColors).hex;
@@ -43,22 +43,34 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+
+    drawPolygon({context, radius: 400, sides: 3})
+
+    context.lineWidth = 30;
+    context.strokeStyle = 'black';
+    context.stroke();
+    context.clip();
+
+
     rects.forEach(rect => {
       const {x, y, w, h, fill, stroke} = rect;
       let shadowColor;
 
       context.save();
+      context.translate(width * -0.5, height * -0.5);
       context.translate(x, y);
       context.strokeStyle = stroke;
       context.fillStyle = fill;
-      context.lineWidth = 20;
+      context.lineWidth = 10;
 
 
 
       drawSkewedRect({context, w, h, degrees});
 
       shadowColor = Color.offsetHSL(fill, 0, 0, -20);
-      shadowColor.rgba[3] = 0.2;
+      shadowColor.rgba[3] = 0.4;
       
       context.shadowColor = Color.style(shadowColor.rgba);
       context.shadowOffsetX = 30;
@@ -68,10 +80,18 @@ const sketch = ({ context, width, height }) => {
       context.fill();
       context.shadowColor = null;
       context.stroke();
+
+      context.globalCompositeOperation = 'source-over';
+
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
       
 
       context.restore();
-    })
+    });
+
+    context.restore();
   };
 };
 
@@ -92,6 +112,20 @@ const drawSkewedRect = ({context, w = 600, h = 200, degrees = 20}) => {
     context.closePath();
   
     context.restore();
+}
+
+const drawPolygon = ({context, radius = 100, sides = 3}) => {
+  const slice = Math.PI * 2 / sides;
+
+  context.beginPath();
+  context.moveTo(0, radius);
+
+  for(let i = 1; i < sides; i++)
+  {
+    const theta = i * slice + Math.PI * 0.5 + 0.4;
+    context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
+  }
+  context.closePath();
 }
 
 canvasSketch(sketch, settings);
